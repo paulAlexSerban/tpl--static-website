e5a52a38927ef76bcb8bdaceef38266c7fa23bad
paulAlexSerban <paulAlexSerban@users.noreply.github.com>
2022-08-18 09:42:35 +0000

    Deploying to gh-pages-static-website from @ paulAlexSerban/tpl--static-website@37fcd4087703e0bcdb95fb1428aad447200e67f7 🚀

---
+++ javascript/entry/main.entry.js
@@ -0,0 +1,17 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ../js-component-library/source/_01_variables/variables.js
+const test = 'test string variabless';
+;// CONCATENATED MODULE: ../js-component-library/source/_03_components/digitalClock.js
+
+const digitalClock = () => {
+  console.log("digital clocks- ".concat(test));
+};
+;// CONCATENATED MODULE: ../js-component-library/source/main.entry.js
+
+digitalClock();
+console.log('hello worldss');
+/******/ })()
+;
---
+++ javascript/entry/second.entry.js
@@ -0,0 +1,5 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+console.log("hello second worlds");
+/******/ })()
+;
